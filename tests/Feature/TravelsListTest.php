43d3cb9d60bf6506d9c1899ac219d80a0391c608
ZylalMinollari <zmiollari@gmail.com>
2023-10-31 10:34:31 +0100

    Test case for travel

--- tests/Feature/TravelsListTest.php
+++ tests/Feature/TravelsListTest.php
@@ -2,21 +2,35 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Travel;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TravelsListTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+
+    public function test_travels_list_returns_pagination()
     {
-        $response = $this->get('/');
+        Travel::factory(16)->create(['is_public' => true]);
+        $response = $this->get('/api/v1/travels');
 
         $response->assertStatus(200);
+        $response->assertJsonCount(15, 'data');
+        $response->assertJsonPath('meta.last_page', 2);
+    }
+
+    public function test_travels_list_shows_only_public_records()
+    {
+
+        $nonPublicTravel = Travel::factory()->create(['is_public' => false]);
+        $publicTravel = Travel::factory()->create(['is_public' => true]);
+
+        $response = $this->get('/api/v1/travels');
+        $response->assertStatus(200);
+        $response->assertJsonCount(1, 'data');
+        $response->assertJsonFragment(['id' => $publicTravel->id]);
+        $response->assertJsonMissing(['id' => $nonPublicTravel->id]);
     }
 }
